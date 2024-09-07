9acba90c602ad87f1306ab506a12627f276adacd
Ali <aliareya2003@gmail.com>
2024-09-07 14:03:20 +0430

    uodate the css

--- js/login-signup.js
+++ js/login-signup.js
@@ -2,19 +2,12 @@ let name = document.getElementById('name');
 let email= document.getElementById('email');
 let pass = document.getElementById('password');
 let cheek= document.getElementById('cheek');
-let btn  = document.getElementById('btn');
-console.log(name);
-console.log(email);
-console.log(pass);
-console.log(cheek);
-console.log(btn);
-
-
-cheek.style.border='2px solid red';
-//create and event for check box
-
-if(cheek.checked){
-    console.log("ok")
-}else {
-    console.log("no")
-}
+let btn  = document.getElementById('si-up-btn');
+btn.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (pass.value === "") {
+        pass.setAttribute('placeholder', "Password Field Cannot be Empty");
+        pass.style.border = '2px solid red';
+        pass.style.color = 'red';
+    }
+});
